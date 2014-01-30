3f49f102061966da1d7e930d0c81217068b6481f
Aleksandr Ponomarenko <mcmegavolt@gmail.com>
2014-01-30 14:03:06 +0200

    deploy update

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,44 +1,25 @@
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
-set :application, 'conference'
-set :repo_url, 'git@github.com:mcmegavolt/confrerences.git'
-
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
-set :branch, 'develop'
-
+set :deploy_to, "/home/conferences/conferences"
+set :scm, :git
+set :deploy_via, :remote_cache
+set :repo_url, 'git@github.com:mcmegavolt/conferences.git'
 set :copy_exclude, [".git"]
-
-# Default deploy_to directory is /var/www/my_app
-set :deploy_to, '/home/conference/conference'
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
+set :branch, 'develop'
+set :format, :pretty
+set :log_level, :debug
+set :pty, true
 
 set :rvm_type, :user
 set :rvm_ruby_version, '2.0.0-p353'
 
-# Default value for :linked_files is []
 # set :linked_files, %w{config/database.yml}
 
-# Default value for linked_dirs is []
 set :linked_dirs, %w{public/uploads}
 
-# Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
 namespace :deploy do
 
