9cf76daab619dad2e330e5fb56fa87401681fe2b
Aleksandr Ponomarenko <mcmegavolt@gmail.com>
2014-01-30 13:01:01 +0200

    many updates

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,8 +2,7 @@
 lock '3.1.0'
 
 set :application, 'conference'
-# set :repo_url, 'git@example.com:me/my_repo.git'
-set :repo_url, '.git'
+set :repo_url, 'git@github.com:mcmegavolt/confrerences.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
