9962dad75e15247fe4384d435b4caa4f75fca103
Aleksandr Ponomarenko <mcmegavolt@gmail.com>
2015-01-30 13:48:54 +0200

    temp uploder fix

--- app/uploaders/article_file_uploader.rb
+++ app/uploaders/article_file_uploader.rb
@@ -14,11 +14,11 @@ class ArticleFileUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  def filename
-  	file_name = I18n.transliterate(model.author).slice(/\w+/).downcase
-  	category = I18n.transliterate(model.category.title).slice(/\w+/).downcase
-  	date = model.conference.start_at.day.to_s + '-' + model.conference.end_at.day.to_s + '.' + model.conference.start_at.month.to_s + '.' + model.conference.start_at.year.to_s
-     "#{file_name}_#{category}_#{date}.#{file.extension}" if original_filename.present?
-  end
+  # def filename
+  # 	file_name = I18n.transliterate(model.author).slice(/\w+/).downcase
+  # 	category = I18n.transliterate(model.category.title).slice(/\w+/).downcase
+  # 	date = model.conference.start_at.day.to_s + '-' + model.conference.end_at.day.to_s + '.' + model.conference.start_at.month.to_s + '.' + model.conference.start_at.year.to_s
+  #    "#{file_name}_#{category}_#{date}.#{file.extension}" if original_filename.present?
+  # end
 
 end
