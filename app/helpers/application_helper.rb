c82067cfd0964f33ea981ca16864e4aa04a85bb2
Alexander Ponomarenko <mcmegavolt@gmail.com>
2017-01-11 11:43:00 +0200

    Add one day conference feature

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
-
-	def show_conference_dates conf
+	def show_conference_dates(conf)
 		render = conf.start_at.day.to_s
-		render += '&ndash;' 
-		render += conf.end_at.day.to_s
-		render += ' ' + l(conf.end_at, format: "%B")
+		unless conf.one_day?
+			render += '&ndash;'
+			render += conf.end_at.day.to_s
+		end
+		render += ' ' + l(conf.end_at, format: '%B')
 		render += ', ' + conf.end_at.year.to_s
 		render.html_safe
 	end
-
 end
--- app/models/conference.rb
+++ app/models/conference.rb
@@ -2,11 +2,12 @@ class Conference < ActiveRecord::Base
 
 	translates :title, :description
 
-	has_many :articles, :dependent => :destroy
+	has_many :articles, dependent: :destroy
+
 	accepts_nested_attributes_for :articles, allow_destroy: true, reject_if: :all_blank
 
-	has_many :categories, :through => :articles
-	
+	has_many :categories, through: :articles
+
 	has_many :advertisings
 
 	validates_presence_of :title, :start_at, :end_at
@@ -16,8 +17,11 @@ class Conference < ActiveRecord::Base
 
 	default_scope order('end_at DESC')
 
-	def has_not_yet_started?
-		Time.zone.now < self.start_at
+	def not_yet_started?
+		Time.zone.now < start_at
 	end
 
+	def one_day?
+		start_at == end_at
+	end
 end
